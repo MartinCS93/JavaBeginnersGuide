a923e870cf822e3796adae7487e4336635574585
MartinCS93 <martin.cristian93@gmai.com>
2022-04-25 21:21:07 +0300

    Initial commit

---
+++ src/Example.java
@@ -0,0 +1,7 @@
+public class Example {
+        //A java program begins with a call to main().
+    public static void main(String[] args) {
+        //This is a simple Java program.
+        System.out.println("Java drives the Web.");
+    }
+}
