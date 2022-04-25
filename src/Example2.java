c4255cd835e86775f84a69c865b5a586b6f741b3
MartinCS93 <martin.cristian93@gmai.com>
2022-04-25 21:34:08 +0300

    another way to declare two or more variables using one statement

--- src/Example2.java
+++ src/Example2.java
@@ -2,6 +2,7 @@ public class Example2 {
     public static void main(String[] args) {
         int myVar1; //this declares a variable
         int myVar2; //this declares another variable
+        //int myVar1, myVar2; - another way to declare two or more variables using one statement
 
         myVar1 = 1024; //this assigns 1024 to myVar1
         System.out.println("myVar1 contains " + myVar1);
