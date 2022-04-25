9a2c37ab71ed5d5344cda2d25a80b6326902cbdf
MartinCS93 <martin.cristian93@gmai.com>
2022-04-25 21:30:56 +0300

    println() / print()

--- src/Example2.java
+++ src/Example2.java
@@ -7,7 +7,7 @@ public class Example2 {
         System.out.println("myVar1 contains " + myVar1);
 
         myVar2 = myVar1 / 2;
-        System.out.println("myVar2 contains myVar1 / 2: ");
+        System.out.print("myVar2 contains myVar1 / 2: ");
         System.out.println(myVar2);
     }
 }
