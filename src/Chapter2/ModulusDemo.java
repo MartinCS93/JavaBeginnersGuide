d6770c2023174059d78eb2a4a9fd0a12084f3169
MartinCS93 <martin.cristian93@gmai.com>
2022-04-30 23:20:34 +0300

    initial commit

---
+++ src/Chapter2/ModulusDemo.java
@@ -0,0 +1,17 @@
+package Chapter2;
+
+//demonstrate the % operator
+public class ModulusDemo {
+    public static void main(String[] args) {
+        int iresult, irem;
+        double dresult, drem;
+
+        iresult = 10 / 3;
+        irem = 10 % 3;
+        System.out.println("Result and remainder of 10/3: " + iresult + " " + irem);
+
+        dresult = 10.0 / 3.0;
+        drem = 10.0 % 3.0;
+        System.out.println("Result and remainder of 10.0/3.0: " + dresult + " " + drem);
+    }
+}
